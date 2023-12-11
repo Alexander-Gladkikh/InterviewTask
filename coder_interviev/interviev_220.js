8791f8a4a63510b6e66b568af22f72b9c58d89d7
Alexander <som17son25@gmail.com>
2023-12-11 19:31:25 +0300

    add incubator

---
+++ coder_interviev/interviev_220.js
@@ -0,0 +1,36 @@
+// Что принимает, что возвращает
+// [1, 2, 3].map(?) ?
+// [1, 2, 3].filter(?) ?
+// [1, 2, 3].some(?) ?
+// [1, 2, 3].every(?) ?
+// [1, 2, 3].includes(?) ?
+//
+// [1, 2, 3].unshift(?) ?
+// [1, 2, 3].slice(?) ?
+// [1, 2, 3].splice(?) ?
+//
+// Object.create(?) ?
+// Object.assign(?) ?
+// Object.prototype.hasOwnProperty(?) ?
+//
+// Function.prototype.call
+// Function.prototype.apply
+// Function.prototype.bind()
+//
+// =====================================================================================
+
+// console.log([] + null + 1)//
+// console.log('foo' + + 'bar') //
+// console.log(!!"false" == !"true")
+
+// ===============================================================================
+
+// console.log("a" && "" && "c")
+// console.log("a" && "" || "c")
+// console.log("" ?? "a")
+
+function Animal() {
+    this.name = 'Alex'
+}
+let animal = new Animal()
+console.log(animal)
---
+++ incubator_js/interviev/05_12_23.js
@@ -0,0 +1,13 @@
+// var result = 6;
+// function foo() {
+//     console.log(result)
+// }
+// function someActionDo() {
+//     var result = 8;
+//     foo();
+//     console.log(result)
+// }
+//
+// someActionDo()
+// ==============================================================
+// пауза 38:16
